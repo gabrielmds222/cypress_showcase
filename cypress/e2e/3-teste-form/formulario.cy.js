e75ddbeb253166fdf1237932ca8b33de69b5006b
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-06-02 11:44:37 -0300

    feat: Testes do tamanho da senha

--- cypress/e2e/3-teste-form/formulario.cy.js
+++ cypress/e2e/3-teste-form/formulario.cy.js
@@ -1,4 +1,11 @@
 describe("Teste do formulário", () => {
+  it("Input string", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("#Nome").should("have.value", "");
+    cy.get("#Nome").type("Gabriel");
+    cy.get("#Nome").should("not.have.value", "");
+  });
+
   it("Verifica preenchimento do campo de email", () => {
     cy.visit("http://localhost:5173/"); // Ajuste o endereço conforme necessário
 
@@ -14,9 +21,9 @@ describe("Teste do formulário", () => {
 
     const senha = "StrongPassword123!"; // Exemplo de senha forte
 
-    cy.get('input[type="password"]').type(senha);
+    cy.get('input[type="password"]').type("12345678");
 
-    cy.get('input[type="password"]').should("have.value", senha);
+    // cy.get('input[type="password"]').should("have.value", senha);
   });
 
   it("Verifica o envio dos valores dos inputs", () => {
@@ -26,7 +33,7 @@ describe("Teste do formulário", () => {
     const senha = "StrongPassword123!"; // Exemplo de senha forte
 
     cy.get('input[type="email"]').type(email);
-    cy.get('input[type="password"]').type(senha);
+    cy.get('input[type="password"]').type("12345678");
 
     cy.get('button[type="submit"]').click();
 
